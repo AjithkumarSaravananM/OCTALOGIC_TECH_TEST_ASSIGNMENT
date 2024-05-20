999974ed20201e51841164cfdb6c972bc4782b86
Ajithkumar <ajithkumar.saravanan@avalara.com>
2024-05-20 13:17:04 +0530

    code fix

--- tests/test_Send_Chat_Message.spec.js
+++ tests/test_Send_Chat_Message.spec.js
@@ -5,20 +5,16 @@ test('Send Chat Message', async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.gotoLoginPage();
     await loginPage.login('demo@minimals.cc', 'demo1234');
-  
+
     await loginPage.chat();
 
-    await page.getByPlaceholder('Search contacts...').click();
-    await page.getByPlaceholder('Search contacts...').fill('deja');
-    await page.waitForTimeout(3000);
-    await page.getByRole('button', { name: 'Deja Brady'}).click();
-  
+    await page.getByRole('button', { name: 'Deja Brady 0 Deja Brady You:' }).click();
 
     await page.getByPlaceholder('Type a message').click();
 
     await page.getByPlaceholder('Type a message').fill('Hello, how are you?');
     await page.getByPlaceholder('Type a message').press('Enter');
-  
+
     const sentMessage = page.getByText('Hello, how are you?', { exact: true });
     await expect(sentMessage).toHaveText('Hello, how are you?');
 });
